b82cc5f701d409ff00b81e1f459c642a0c21a36c
SathishMadesh <s747272946@gmail.com>
2024-01-22 18:33:30 +0530

    update

--- DSA_tasks/list_2.py
+++ DSA_tasks/list_2.py
@@ -2,11 +2,6 @@
 # heros=['spider man','thor','hulk','iron man','captain america']
 # Using this find out,
 
-# 4. Now you don't like thor and hulk because they get angry easily :)
-#    So you want to remove thor and hulk from list and replace them with doctor strange (because he is cool).
-#    Do that with one line of code.
-# 5. Sort the heros list in alphabetical order (Hint. Use dir() functions to list down all functions available in list)
-
 
 heros=['spider man','thor','hulk','iron man','captain america']
 
@@ -24,4 +19,16 @@ print(heros)
 # 3. You realize that you need to add 'black panther' after 'hulk', so remove it from the list first and then add it after 'hulk'
 new_hero = heros.pop()
 heros.insert(3, new_hero) #3 is an zero-based index (position)
+print(heros)
+
+
+# 4. Now you don't like thor and hulk because they get angry easily :)
+#    So you want to remove thor and hulk from list and replace them with doctor strange (because he is cool).
+#    Do that with one line of code.
+heros[1:3] = ['doctor strange']
+print(heros)
+
+
+# 5. Sort the heros list in alphabetical order (Hint. Use dir() functions to list down all functions available in list)
+heros.sort()
 print(heros)
